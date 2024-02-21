2eb5ffb6d357d414f1f0603a44a36c55840a993d
faizanjoya <faizanjoya@outlook.com>
2024-02-21 10:57:05 +0000

    vitest jestdom setup

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,3 +1,6 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
@@ -6,5 +9,16 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3001,
-  }
+  },
+  test: {
+    // wont need to import it, describe in test files
+    globals: true,
+    // testing web app stuff, otherwise may default to nodejs
+    environment: 'jsdom',
+    setupFiles: './src/test/setup.ts',
+    // you might want to disable it, if you don't have tests that rely on CSS
+    // since parsing CSS is slow
+    // .toBeVisible may need it
+    css: true,
+  },
 })
